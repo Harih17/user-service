25739222a741c6bc5c86c1034bb922c8fe684501
shreysmh <shreya.maudgilya@gmail.com>
2024-10-05 15:01:46 +0530

    Removed sql file

--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE user_details (
-    serial_code INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    age INT NOT NULL,
-    dateOfBirth VARCHAR(50) NOT NULL,
-    address VARCHAR(100) NOT NULL,
-    phoneNumber VARCHAR(12) NOT NULL,
-    email VARCHAR(50) NOT NULL
-);
