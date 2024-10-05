786096856524fc7c4c71ff098a06ad00fa159652
Harih17 <harimayilswamy@gmail.com>
2024-10-05 13:38:24 +0530

    updated class names

--- src/test/java/com/spring/learn/concepts/UserServiceApplicationTests.java
+++ src/test/java/com/spring/learn/concepts/UserServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConceptsApplicationTests {
+class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
