975e32d521b03cf25f9891d4cb812725f2ff52fd
Harih17 <harimayilswamy@gmail.com>
2024-10-05 13:04:47 +0530

    Added constants file

---
+++ src/main/java/com/spring/learn/concepts/constants/UserConstants.java
@@ -0,0 +1,11 @@
+package com.spring.learn.concepts.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserConstants {
+
+
+
+}
